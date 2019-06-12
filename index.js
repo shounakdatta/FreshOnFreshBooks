5bbd392993004144b58bb45783b7d469ec7290e2
Shounak Datta <shounakdatta@Shounaks-MacBook-Pro.local>
2019-06-12 13:56:03 -0400

    Added token fetch

--- index.js
+++ index.js
@@ -7,11 +7,27 @@ let authCode = null;
 app.get('/', async (req, res) => {
     const { code } = req.query
 
-    if (code) {
+    if (!authCode && code) {
         authCode = code
-        return res.sendFile(path.join(__dirname + '/tokens.html'))
+        const tokenObj = await fetch(
+            'https://api.freshbooks.com/auth/oauth/token',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    grant_type: "authorization_code",
+                    client_secret: "30aaf2abd5a8c70dd49bc1aff87743626c1c178b68e815d441669f64ab075832",
+                    code,
+                    client_id: "4d9b451a98341639ac89b962529888e75000493c3776045af1864508f1342901",
+                    redirect_uri: "https://node-on-freshbooks.herokuapp.com/"
+                })
+            }
+        );
+        console.dir(tokenObj)
     }
-    return res.sendFile(path.join(__dirname + '/authorize.html'))
+    return res.send(File(path.join(__dirname + '/authorize.html')))
 })
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
