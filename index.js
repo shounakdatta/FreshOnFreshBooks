2a93e0d06b39ed945b52964942fc0abae57e6aa0
Shounak Datta <shounakdatta@Shounaks-MacBook-Pro.local>
2019-06-12 11:38:54 -0400

    Init heroku

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 4000
+
+app.get('/', async (req, res) => {
+    return res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
