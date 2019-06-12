b00d8f99fd8efdd6c431102b91ff3134ce6bb55b
Shounak Datta <shounakdatta@Shounaks-MacBook-Pro.local>
2019-06-12 11:47:33 -0400

    Fixed env port

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ app.get('/', async (req, res) => {
     return res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.port || port, () => console.log(`Example app listening on port ${port}!`))
