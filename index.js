b8f40ea89c55126e26117dc0392e0c674b9b9221
Shounak Datta <shounakdatta@Shounaks-MacBook-Pro.local>
2019-06-12 11:53:00 -0400

    Fixed env port 4

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = process.env.port ? process.env.port : 4000
 
 app.get('/', async (req, res) => {
     return res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || 4000, () => console.log(`Example app listening on port ${port}!`))
