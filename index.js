48a9e45d6246b73d1390f2224b249290ea37dd65
Shounak Datta <shounakdatta@Shounaks-MacBook-Pro.local>
2019-06-12 12:10:40 -0400

    Store Authorization Code

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 4000
+let authCode = null;
 
 app.get('/', async (req, res) => {
-    console.log("test", req.query);
+    const { code } = req.query
 
-    return res.sendFile(path.join(__dirname + '/index.html'))
+    if (code) {
+        authCode = code
+        return res.sendFile(path.join(__dirname + '/tokens.html'))
+    }
+    return res.sendFile(path.join(__dirname + '/authorize.html'))
 })
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
