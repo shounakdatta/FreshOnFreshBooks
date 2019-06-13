5e0d8435efe4c71831aad48f52c5d349243c7742
shounakdatta <shounak14datta@gmail.com>
2019-06-13 13:26:03 -0400

    Removed third party oauth library

--- index.js
+++ index.js
@@ -12,16 +12,6 @@ const redirectUri = 'https://node-on-freshbooks.herokuapp.com/'
 let authCode = null
 let tokenObj = null
 
-// const ClientOAuth2 = require('client-oauth2')
-
-// const clientAuth = new ClientOAuth2({
-//     clientId: '4d9b451a98341639ac89b962529888e75000493c3776045af1864508f1342901',
-//     clientSecret: '30aaf2abd5a8c70dd49bc1aff87743626c1c178b68e815d441669f64ab075832',
-//     accessTokenUri: 'https://api.freshbooks.com/auth/oauth/token',
-//     authorizationUri: 'https://my.freshbooks.com/service/auth/oauth/authorize',
-//     redirectUri: 'https://node-on-freshbooks.herokuapp.com/'
-// })
-
 app.get('/', (req, res) => {
     const { code } = req.query
 
