5d74bd4c3c3d4a5d82b6b416f1e8f5e24d27bd72
shounakdatta <shounak14datta@gmail.com>
2019-06-13 11:01:39 -0400

    Imported node fetch

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 5000
+const fetch = require('node-fetch')
+
+const clientId = '4d9b451a98341639ac89b962529888e75000493c3776045af1864508f1342901'
+const clientSecret = '30aaf2abd5a8c70dd49bc1aff87743626c1c178b68e815d441669f64ab075832'
+const accessTokenUri = 'https://api.freshbooks.com/auth/oauth/token'
+const authorizationUri = 'https://my.freshbooks.com/service/auth/oauth/authorize'
+const redirectUri = 'https://node-on-freshbooks.herokuapp.com/'
 let authCode = null
 
-const ClientOAuth2 = require('client-oauth2')
+// const ClientOAuth2 = require('client-oauth2')
 
-const clientAuth = new ClientOAuth2({
-    clientId: '4d9b451a98341639ac89b962529888e75000493c3776045af1864508f1342901',
-    clientSecret: '30aaf2abd5a8c70dd49bc1aff87743626c1c178b68e815d441669f64ab075832',
-    accessTokenUri: 'https://api.freshbooks.com/auth/oauth/token',
-    authorizationUri: 'https://my.freshbooks.com/service/auth/oauth/authorize',
-    redirectUri: 'https://node-on-freshbooks.herokuapp.com/'
-})
+// const clientAuth = new ClientOAuth2({
+//     clientId: '4d9b451a98341639ac89b962529888e75000493c3776045af1864508f1342901',
+//     clientSecret: '30aaf2abd5a8c70dd49bc1aff87743626c1c178b68e815d441669f64ab075832',
+//     accessTokenUri: 'https://api.freshbooks.com/auth/oauth/token',
+//     authorizationUri: 'https://my.freshbooks.com/service/auth/oauth/authorize',
+//     redirectUri: 'https://node-on-freshbooks.herokuapp.com/'
+// })
 
 app.get('/', (req, res) => {
     const { code } = req.query
@@ -25,16 +32,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/authorize/', (req, res) => {
-    const uri = clientAuth.code.getUri()
+    let uri = authorizationUri + "?client_id=" + clientId +
+        "&redirect_uri" + redirectUri + "scope=&response_type=code&state="
     res.redirect(uri)
 })
 
 app.get('/api/tokens/', async (req, res) => {
-    console.log("clientAuth");
-    const request = { ...req, query: { code: authCode } }
-    await clientAuth.code.getToken(request)
-        .then(user => res.send(JSON.stringify({ success: true, ...user })))
-        .catch(err => res.send(JSON.stringify({ success: false, ...err })))
+    // console.log("clientAuth");
+    // const request = { ...req, query: { code: authCode } }
+    // await clientAuth.code.getToken(request)
+    //     .then(user => res.send(JSON.stringify({ success: true, ...user })))
+    //     .catch(err => res.send(JSON.stringify({ success: false, ...err })))
 })
 
 
